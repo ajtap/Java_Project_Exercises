f0e0d65d5dbe8b7ee2a6544fdd4aae3249ba0112
jryantap <jhericoryan.tapang@gmail.com>
2018-07-24 18:35:51 -0400

    added CS212 question comments.

--- src/question3CS212.java
+++ src/question3CS212.java
@@ -10,51 +10,70 @@ class testingCode {
         int b[] = {3,1,4,1};
         int x = 1, y = 2;
 
+        // (a) Returns true if at least one of x and y is positive. Here "Yes" is printed.
         if (positive(x,y)) System.out.println("Yes");
 
+        // (b) Return the sum of the first row. Here 10 is printed.
         System.out.println(rowSum(a));
 
+        // (c) Return the smallest element. Here 1 is printed.
         System.out.println(smallest(b));
 
+        // (d) Remove the first letter. Here "ello" is printed.
         System.out.println(removeFirst(s));
 
+        // (e) Insert an 'X' at the specified position. Here "HeXllo" is printed.
         s = addX(s, 2);
         System.out.println(s);
 
     }
-
+    // (a)
     private static boolean positive(int x, int y) {
+        // Declares a boolean variable to return later.
         boolean state = false;
+        // Checks if either x or y is positive (greater than 0), changes "state" to true if so.
         if (x > 0 || y > 0) state = true;
+        // Returns the result of "state".
         return state;
     }
-
+    // (b)
     private static int rowSum(int[][] a) {
-
+        // Declares an integer variable to hold the sum value.
         int sum = 0;
+        /* Iterates, from left to right (which uses 'j' instead of 'i'), in the 2D array to add the first
+        group of values. */
         for (int j = 0; j < 4; j++) {
+            // Adds each value to "sum".
             sum = sum + a[0][j];
         }
+        // Returns the final result of "sum".
         return sum;
     }
-
+    // (c)
     private static int smallest(int[] b) {
-
+        // Sorts array "b" automatically from smallest to largest.
         Arrays.sort(b);
+        // Returns the first value of array "b", which is 1.
         return b[0];
     }
-
+    // (d)
     private static String removeFirst(String s) {
-
+        /* Makes a StringBuilder variable to hold the original string, allowing the use of different syntax
+        that can properly edit the string. */
         StringBuilder x = new StringBuilder(s);
+        // Deletes the first letter of "Hello".
         x = x.deleteCharAt(0);
+        // Returns the result of the deletion.
         return s = x.toString();
     }
-
+    // (e)
     private static String addX(String s, int i) {
-
+        /* Makes a StringBuilder variable to hold the original string, allowing the use of different syntax
+        that can properly edit the string. */
         StringBuilder x = new StringBuilder(s);
+        // Inserts the character 'X' at position 'i'.
         x = x.insert(i, 'X');
+        // Returns the result of the insertion, "HeXllo".
         return x.toString();
     }
 }
--- src/question4CS212.java
+++ src/question4CS212.java
@@ -1,15 +1,21 @@
 class PartyHandshakes {
 
     public static void main(String[] args) {
-
+        // Declares var 'n' for the number of people (set to 10) and var
+        // 'total_handshakes' for the total amount of handshakes (set to 'n' for iteration purposes).
         int n = 10;
         int total_handshakes = n;
 
+        // Prints the # of people.
         System.out.println("There are " + n + " people at the party.");
+        // This 'for' loop uses var 'i' to slowly increase the total number of handshakes similarly to
+        // a factorial function (however, instead of multiplying we are adding the handshakes).
         for (int i = n - 1; i > 0; i--) {
             total_handshakes = total_handshakes + i;
+            // Prints value of 'i' through each loop for testing.
             System.out.println(i);
         }
+        // Prints the result amount of handshakes, 55.
         System.out.println("There were " + total_handshakes + " handshakes at the party.");
     }
 }
