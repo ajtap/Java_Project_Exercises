f1228712432b428fa8bfd0c91ad460bf203e5c07
jryantap <jhericoryan.tapang@gmail.com>
2018-08-03 13:34:04 -0400

    added comments to variables to clear up confusion, as well as constructor frames and toString() override frames.

--- src/SimplifiedAirlineReservationSystemC212.java
+++ src/SimplifiedAirlineReservationSystemC212.java
@@ -4,11 +4,22 @@ import java.util.*;
 class Passenger {
 
     //Instance variables.
-    ArrayList<Ticket> myTickets = new ArrayList<Ticket>();
+    ArrayList<Ticket> myTickets = new ArrayList<Ticket>(); //Contains each ticket that the user books.
+    //The below variables are user-input, and are the info of the user's passenger.
     String firstName;
     String lastName;
     String address;
     String phone;
+
+    //Constructor for class "Passenger".
+    public Passenger() {
+
+    }
+
+    //Overrides toString() method for testing.
+    public String toString() {
+
+    }
 
     void cancel(Ticket t) {
         //Cancel's the passenger's ticket 't'.
@@ -31,23 +42,45 @@ class Passenger {
 class Ticket {
 
     //Instance variables.
-    static int counter = 0;
-    int ticketNumber;
-    double price;
-    String myAirline;
-    String myPassenger;
-    String myFlight;
+    static int counter = 0; //Keeps track of the # of tickets the user booked, and gives each ticket a unique number.
+    //The below variables contain info about each ticket that is booked.
+    int ticketNumber; //Unique ticket #
+    double price; //Ticket price
+    String myAirline; //The airline the ticket was issued from.
+    String myPassenger; //The name of the ticket owner (user).
+    String myFlight; //The name of the flight the ticket books.
+
+    //Constructor for class "Ticket".
+    public Ticket() {
+
+    }
+
+    //Overrides toString() method for testing.
+    public String toString() {
+
+    }
 
     void cancel() {
         //Cancels the ticket.
+        if ()
     }
 }
 
 class Airline {
 
     //Instance variables.
-    ArrayList<Flight> flights = new ArrayList<Flight>();
-    String name;
+    ArrayList<Flight> flights = new ArrayList<Flight>(); //Contains each flight within the airline.
+    String name; //Contains the airline name.
+
+    //Constructor for class "Airline".
+    public Airline() {
+
+    }
+
+    //Overrides toString() method for testing.
+    public String toString() {
+
+    }
 
     void cancel(Ticket t) {
         //Cancels a ticket.
@@ -56,6 +89,7 @@ class Airline {
     void issueRefund(Ticket t) {
         /*Issues a refund --- it just prints a message to the screen about which passenger has been credited
         how much money since in this simulation we will not keep track of bank balances for passengers or airlines.*/
+        System.out.println(t.myPassenger + " has been issued $" + t.price + " as a refund for a canceled ticket.");
     }
 
     ArrayList<Flight> findFlights(String date, double time, String origin) {
@@ -79,19 +113,27 @@ class Airline {
 class Flight {
 
     //Instance variables.
-    ArrayList<Ticket> tickets = new ArrayList<Ticket>();
-    static int counter = 0;
-    int seats;
-    int filledSeats;
-    int flightLength;
-    int flightNumber;
-    String airline;
-    String originAirport;
+    ArrayList<Ticket> tickets = new ArrayList<Ticket>(); //Contains a list of the available tickets.
+    static int counter = 0; //Keeps track of the # of flights, and gives each flight a unique number.
+    int seats; //The # of seats on each flight.
+    int filledSeats = 0; //The # of filled seats on each flight.
+    int flightLength; //The approximate time the flight will last.
+    int flightNumber; //Unique flight number.
+    String airline; //Name of the airline.
+    String originAirport; //Current location of the flight.
     String destination;
-    //Unknown Types?
-    int numTickets;
-    String date;
+    String date; //Date of the flight's departure.
     String departureTime;
+
+    //Constructor for class "Flight".
+    public Flight() {
+
+    }
+
+    //Overrides toString() method for testing.
+    public String toString() {
+
+    }
 
     boolean matches(String d, double t, String from) {
         //Does the flight match date 'd', time 't' and originAirport 'from' to within a 4 hour departure window.
