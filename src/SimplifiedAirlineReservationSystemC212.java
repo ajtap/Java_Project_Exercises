767d6068eec6a2f9d265d3130c5ad88fd51957ab
jryantap <jhericoryan.tapang@gmail.com>
2018-08-30 16:36:25 -0400

    added to the get

--- src/SimplifiedAirlineReservationSystemC212.java
+++ src/SimplifiedAirlineReservationSystemC212.java
@@ -214,8 +214,6 @@ class Airline {
     double cost(Flight f) {
         //Gives the cost of a ticket for a particular flight. Devise your own sensible pricing policy so that
         //tickets get more expensive as a flight fills up.
-
-        f.getCost();
     }
 
     void createFlight(double time, int numSeats, String from, String to) {
@@ -357,6 +355,8 @@ class Flight {
 
     double getCost() {
         //Use the flight's airline's method to generate the cost of the next ticket for this flight.
+        int start_cost = 150;
+        return start_cost + start_cost * (getFilledSeats());
     }
 }
 
